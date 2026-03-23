be17c8ebb929c5d2a6eabbb2796e20bc30eb9e5d
mefranklin6 <mefranklin@csuchico.edu>
2026-03-23 08:38:44 -0700

    move metadata from AssemblyInfo to .csproj file to modernize

--- SOURCE/Properties/AssemblyInfo.cs
+++ SOURCE/Properties/AssemblyInfo.cs
@@ -1,18 +1,4 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
-
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("AudioDeviceCmdlets")]
-[assembly: AssemblyDescription("AudioDeviceCmdlets is a suite of PowerShell Cmdlets to control audio devices on Windows")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("frgnca")]
-[assembly: AssemblyProduct("AudioDeviceCmdlets")]
-[assembly: AssemblyCopyright("(c) 2016-2022 Francois Gendron <fg@frgn.ca>")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+﻿using System.Runtime.InteropServices;
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
@@ -21,13 +7,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("7156b1c0-8e86-4d19-8df1-058c15629f36")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-[assembly: AssemblyVersion("3.1.0.2")]
-[assembly: AssemblyFileVersion("3.1.0.2")]
